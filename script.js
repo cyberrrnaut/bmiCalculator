8b014c7c3aca91d6b1f3bdd3a12aa408e0383aa5
Soumyendu <cyberrrnaut@gmail.com>
2023-12-10 20:03:51 +0530

    minor bug fixes

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ form.addEventListener('submit',function(s){
 
         results.innerHTML = "Please Give a valid Weight";
        } else{
-              const bmi=  (weight/((height*height)/1000)).toFixed(2);
+              const bmi=  (weight/((height*height)/10000)).toFixed(2);
 
               results.innerHTML= `<span>${bmi}</span>`
 
        }
-       const bmi=  (weight/((height*height)/1000)).toFixed(2);
+       const bmi=  (weight/((height*height)/10000)).toFixed(2);
        if(bmi< 18.6){
         resultComment.innerHTML="Under Weight";
        }else if(bmi>= 18.6 && bmi<24.9){
